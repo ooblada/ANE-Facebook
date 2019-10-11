c9d745130db9741418830a4ce67d1bf9ec18ed47
hdelaire <hugues.delaire@ooblada.com>
2019-10-11 12:34:52 +0200

    Updating ios pods

--- ios/Pods/FBSDKShareKit/FBSDKShareKit/FBSDKShareKit/FBSDKSharingContent.h
+++ ios/Pods/FBSDKShareKit/FBSDKShareKit/FBSDKShareKit/FBSDKSharingContent.h
@@ -19,7 +19,7 @@
 #import <Foundation/Foundation.h>
 
 #import <FBSDKCoreKit/FBSDKCopying.h>
-#import <FBSDKShareKit/FBSDKSharingValidation.h>
+#import <FBSDKShareKit/FBSDKShareKit/FBSDKSharingValidation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
